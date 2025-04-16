bd0aa29d7b3f4d5cf4132c5098e41e14a41b0bdb
S4nti4goCoder <santiagoquintero.softdev.code@gmail.com>
2025-04-16 00:58:24 -0500

    feat(api): The .htaccess file is created, the CORS are configured, the GET, POST, PUT, DELETE requests are tested, the connection to the database is made and a GET request is made without filter.

--- api/routes/route.php
+++ api/routes/route.php
@@ -1,8 +1,81 @@
 <?php
 
-$json = array(
-    'status' => 200,
-    "result" => "success"
-);
-echo json_encode($json);
-return;
+$routesArray = explode("/", $_SERVER['REQUEST_URI']);
+$routesArray = array_filter($routesArray);
+
+/*=============================================
+Cuando no se hace ninguna petición a la API
+=============================================*/
+if (count($routesArray) == 0) {
+    $json = array(
+        'status' => 404,
+        "results" => "Not found"
+    );
+
+    echo json_encode($json, http_response_code($json["status"]));
+    return;
+} else {
+
+    /*=============================================
+	Peticiones GET
+	=============================================*/
+    if (
+        count($routesArray) == 1 &&
+        isset($_SERVER["REQUEST_METHOD"]) &&
+        $_SERVER["REQUEST_METHOD"] == "GET"
+    ) {
+        $response = new GetController();
+        $response -> getData($routesArray[1]);
+    }
+
+    /*=============================================
+	Peticiones POST
+	=============================================*/
+    if (
+        count($routesArray) == 1 &&
+        isset($_SERVER["REQUEST_METHOD"]) &&
+        $_SERVER["REQUEST_METHOD"] == "POST"
+    ) {
+        $json = array(
+            'status' => 200,
+            "results" => "POST"
+        );
+
+        echo json_encode($json, http_response_code($json["status"]));
+        return;
+    }
+
+    /*=============================================
+	Peticiones PUT
+	=============================================*/
+    if (
+        count($routesArray) == 1 &&
+        isset($_SERVER["REQUEST_METHOD"]) &&
+        $_SERVER["REQUEST_METHOD"] == "PUT"
+    ) {
+        $json = array(
+            'status' => 200,
+            "results" => "PUT"
+        );
+
+        echo json_encode($json, http_response_code($json["status"]));
+        return;
+    }
+
+    /*=============================================
+	Peticiones DELETE
+	=============================================*/
+    if (
+        count($routesArray) == 1 &&
+        isset($_SERVER["REQUEST_METHOD"]) &&
+        $_SERVER["REQUEST_METHOD"] == "DELETE"
+    ) {
+        $json = array(
+            'status' => 200,
+            "results" => "DELETE"
+        );
+
+        echo json_encode($json, http_response_code($json["status"]));
+        return;
+    }
+}
