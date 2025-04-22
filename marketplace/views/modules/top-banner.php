d35a5945d9112494892f700c38bc5869481eafc2
S4nti4goCoder <santiagoquintero.softdev.code@gmail.com>
2025-04-22 00:59:18 -0500

    feat: Navigating through the internal pages of the Marketplace and the 404 Page is created.

--- marketplace/views/modules/top-banner.php
+++ marketplace/views/modules/top-banner.php
@@ -1,8 +1,9 @@
-<?php 
+<?php
 
 $randomId = rand(1, $totalProducts);
 
-$url = CurlController::api()."relations?rel=products,categories&type=product,category&linkTo=id_product&equalTo=".$randomId;
+
+$url = CurlController::api() . "relations?rel=products,categories&type=product,category&linkTo=id_product&equalTo=" . $randomId;
 $method = "GET";
 $fields = array();
 $header = array();
@@ -12,7 +13,7 @@ $topBanner = json_decode($randomProduct->top_banner_product, true);
 
 ?>
 
-<div class="ps-block--promotion-header bg--cover"  style="background: url(img/products/<?php echo $randomProduct->url_category ?>/top/<?php echo $topBanner["IMG tag"] ?>);">
+<div class="ps-block--promotion-header bg--cover" style="background: url(img/products/<?php echo $randomProduct->url_category ?>/top/<?php echo $topBanner["IMG tag"] ?>);">
     <div class="container">
         <div class="ps-block__left">
             <h3><?php echo $topBanner["H3 tag"] ?></h3>
@@ -23,6 +24,6 @@ $topBanner = json_decode($randomProduct->top_banner_product, true);
         </div>
         <div class="ps-block__center">
             <p><?php echo $topBanner["P2 tag"] ?><span><?php echo $topBanner["Span tag"] ?></span></p>
-        </div><a class="ps-btn ps-btn--sm" href="<?php echo $path.$randomProduct->url_product ?>"><?php echo $topBanner["Button tag"] ?></a>
+        </div><a class="ps-btn ps-btn--sm" href="<?php echo $path . $randomProduct->url_product ?>"><?php echo $topBanner["Button tag"] ?></a>
     </div>
 </div>
